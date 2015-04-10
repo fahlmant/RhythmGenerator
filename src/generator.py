082d23172fba4a36d262ceea6ee0b39ce6228d3c
fahlmant <fahlmantaylor@gmail.com>
2015-04-10 09:38:58 +0000

    Ran flake8, fixed some style issues

--- src/generator.py
+++ src/generator.py
@@ -1,31 +1,43 @@
+import os
+import random
+
 
 class Measures:
-    total_beats = 0
-    notes = [Elements() for i in range (total_beats)]
+
     def __init__(self, beats):
-        self.notes = []
         self.total_beats = beats
-    def add_element(Element):
-        notes += Element
-        if (total_beats > 0):
-            total_beats -= Element.length
+        self.notes = [Element() for i in range(self.total_beats)]
+
+    def add_element(self, Element):
+        self.notes += Element
+        if (self.total_beats > 0):
+            self.total_beats -= Element.length
+
 
 class Element:
+
     type = 'Element'
     length = 0
+
     def __init__(self, type, length):
         self.type = type
         self.length = length
 
+
 class Note(Element):
-   type = 'Note'
-   length = 0
+
+    type = 'Note'
+    length = 0
+
 
 class Rest(Element):
+
     type = 'Rest'
-    length = 0;
+    length = 0
+
 
 def run():
+
     for i in range(8):
         for j in range(16):
             #Pick random length
@@ -35,12 +47,22 @@ def run():
             j += 1
         i+= 1
 
+
 def print_generation():
+
     for i in range(8):
         #print each measure
         i+= 1
+y7
 
 def main():
+
+    random.seed()
+    measure_list = []
+    for i in range(0, 8):
+        measure_list.append('hi')
     run()
 
-if __name__ == "__main__": main()
+
+if __name__ == "__main__":
+    main()
