1ff676fde581825893237007e5b93cd587dece28
Taylor Fahlman <fahlmantaylor@gmail.com>
2017-05-07 09:02:48 -0700

    Add flask server

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+import os
+import sys
+import subprocess
+import generator
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world(name=None):
+    generator.main()
+    subprocess.Popen('lilypond -f png something.ly', shell=True)
+    return render_template('hello.html', name=name)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
