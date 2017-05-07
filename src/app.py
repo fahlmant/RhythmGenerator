2b9790e4f9a9b3065bb37f212006863cf28581fc
Taylor Fahlman <fahlmantaylor@gmail.com>
2017-05-07 09:05:43 -0700

    Add html template

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def hello_world(name=None):
     generator.main()
     subprocess.Popen('lilypond -f png something.ly', shell=True)
-    return render_template('hello.html', name=name)
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
